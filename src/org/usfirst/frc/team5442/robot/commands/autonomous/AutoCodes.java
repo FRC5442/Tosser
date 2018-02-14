9d6209eb95256448c260b9e9c40299f9c69464cd
Travis Unruh <travis.unruh@samaritanministries.org>
2018-02-13 18:03:24 -0600

    Enum for AutoCodes.

---
+++ src/org/usfirst/frc/team5442/robot/commands/autonomous/AutoCodes.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team5442.robot.commands.autonomous;
+
+public enum AutoCodes {
+	Undefined,
+	LeftSide_Baseline,
+	LeftSide_LeftSwitch,
+	LeftSide_RightSwitch,
+	LeftSide_LeftScale,
+	LeftSide_RightScale,
+	Middle_BaseLine_Left,
+	Middle_BaseLine_Right,
+	Middle_LeftSwitch,
+	Middle_RightSwitch,
+	RightSide_Baseline,
+	RightSide_LeftSwitch,
+	RightSide_RightSwitch,
+	RightSide_LeftScale,
+	RightSide_RightScale
+}
