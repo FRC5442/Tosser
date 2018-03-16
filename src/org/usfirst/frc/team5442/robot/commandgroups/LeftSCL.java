a51da1e2fda03ac829fc0c47fc4bd022f778246a
SamBealmer <sjbealmer@wacohi.net>
2018-03-16 13:11:16 -0500

    push

--- src/org/usfirst/frc/team5442/robot/commandgroups/LeftSCL.java
+++ src/org/usfirst/frc/team5442/robot/commandgroups/LeftSCL.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team5442.robot.commands.CompressorStop;
 import org.usfirst.frc.team5442.robot.commands.FlipAuto;
 import org.usfirst.frc.team5442.robot.commands.PIDDriveCommand;
 import org.usfirst.frc.team5442.robot.commands.PIDTurnCommand;
+import org.usfirst.frc.team5442.robot.commands.PincerToggle;
 import org.usfirst.frc.team5442.robot.commands.Shoot;
 import org.usfirst.frc.team5442.robot.commands.Stop;
 
@@ -13,13 +14,14 @@ public class LeftSCL extends CommandGroup{
 
 	public LeftSCL() {
 		//mostly done, check distances, 
-		addSequential(new PIDDriveCommand(25, 0.7));
-		addSequential(new Stop(-.30));
+		addSequential(new PIDDriveCommand(324, 0.5));
+		addSequential(new Stop(-.20));
 		addSequential(new PIDTurnCommand(90));
-		addSequential(new PIDDriveCommand(-5, .85));
+		addSequential(new PIDDriveCommand(-25, .85));
 		addSequential(new CompressorStop());
-		addSequential(new Reload());
-		addSequential(new FlipAuto(.25, 1));
+		//addSequential(new Reload());
+		addSequential(new PincerToggle());
+		addSequential(new FlipAuto(.5, 1));
 		addSequential(new Shoot());
 	}
 
